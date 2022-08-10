a34500c4395189b4c0d9c4aeb0bee8122c9705a5
ju1u <research.ju1u@gmail.com>
2022-08-10 23:43:26 +0900

    Add AOP Proxy Pattern Source

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,15 @@ import com.ju1u.design.Adapter.Airconditioner;
 import com.ju1u.design.Adapter.Cleaner;
 import com.ju1u.design.Adapter.Electronic110V;
 import com.ju1u.design.Adapter.SocketAdapter;
+import com.ju1u.design.Aop.AopBrowser;
+import com.ju1u.design.Proxy.Browser;
+import com.ju1u.design.Proxy.BrowserProxy;
+import com.ju1u.design.Proxy.IBrowser;
 import com.ju1u.design.singleton.Aclazz;
 import com.ju1u.design.singleton.Bclazz;
 import com.ju1u.design.singleton.SocketClient;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main{
     public static void main(String[] args){
@@ -25,19 +31,51 @@ public class Main{
          * Adapter Pattern
          */
 
-        Airconditioner airconditioner = new Airconditioner();
-        Cleaner cleaner = new Cleaner();
-        Electronic110V socketAdapter = new SocketAdapter(airconditioner);
+//        Airconditioner airconditioner = new Airconditioner();
+//        Cleaner cleaner = new Cleaner();
+//        Electronic110V socketAdapter = new SocketAdapter(airconditioner);
+//
+//        connect(cleaner);
+//        connect(socketAdapter);
 
-        connect(cleaner);
-        connect(socketAdapter);
+        /**
+         * Proxy Pattern
+         */
+        IBrowser iBrowser = new Browser("www.naver.com");
+        IBrowser iBrowser2 = new BrowserProxy("www.naver.com");
+        iBrowser.show();
+        iBrowser.show();
+        iBrowser2.show();
+        iBrowser2.show();
 
+        /**
+         * AOP Proxy Pattern
+         */
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+        IBrowser iBrowser3 = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    System.out.println("after");
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                });
+        iBrowser3.show();
+        System.out.println("loading time: " + end.get());
+        iBrowser3.show();
+        System.out.println("loading time: " + end.get());
+        iBrowser3.show();
+        System.out.println("loading time: " + end.get());
     }
+
 
     /**
      * Adapter Pattern
      */
-    public static void connect(Electronic110V electronic110V){
-        electronic110V.powerOn();
-    }
+//    public static void connect(Electronic110V electronic110V){
+//        electronic110V.powerOn();
+//    }
 }
